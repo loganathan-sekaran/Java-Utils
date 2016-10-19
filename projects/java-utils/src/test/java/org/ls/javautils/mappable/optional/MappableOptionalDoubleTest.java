d1d46c4e6a0d68c379229f22acd8b29110a6b553
Loganathan S <loganathans@ilink-systems.com>
2016-10-20 00:03:39 +0530

    More test cases.

--- projects/java-utils/src/test/java/org/ls/javautils/mappable/optional/MappableOptionalDoubleTest.java
+++ projects/java-utils/src/test/java/org/ls/javautils/mappable/optional/MappableOptionalDoubleTest.java
@@ -2,6 +2,7 @@ package org.ls.javautils.mappable.optional;
 
 import java.util.OptionalDouble;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.DoubleFunction;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -25,5 +26,41 @@ public class MappableOptionalDoubleTest  {
 		Assert.assertNotNull(mappableOptionalDouble);
 		Assert.assertNotNull(mappableOptionalDouble.getOptionalDouble());
 		Assert.assertEquals(d, mappableOptionalDouble.getOptionalDouble().getAsDouble(),0d);
+	}
+
+	@Test
+	public void testEmpty() throws Exception {
+		MappableOptionalDouble mappableOptionalDouble = MappableOptionalDouble.empty();
+		Assert.assertNotNull(mappableOptionalDouble);
+		Assert.assertNotNull(mappableOptionalDouble.getOptionalDouble());
+		Assert.assertFalse(mappableOptionalDouble.getOptionalDouble().isPresent());
+	}
+
+	@Test
+	public void testGetOptionalDouble() throws Exception {
+		double d = ThreadLocalRandom.current().nextDouble();
+		MappableOptionalDouble mappableOptionalDouble = MappableOptionalDouble.of(d);
+		Assert.assertNotNull(mappableOptionalDouble);
+		Assert.assertNotNull(mappableOptionalDouble.getOptionalDouble());
+		Assert.assertEquals(d, mappableOptionalDouble.getOptionalDouble().getAsDouble(),0d);
+	}
+
+	@Test
+	public void testFlatMapAsMappableOptionalDouble() throws Exception {
+		double d = ThreadLocalRandom.current().nextDouble();
+		MappableOptionalDouble mod1 = MappableOptionalDouble.of(d);
+		DoubleFunction<MappableOptionalDouble> mapper = d1 -> MappableOptionalDouble.of(d1 * 2);
+		MappableOptionalDouble mod2 = mod1.flatMapAsMappableOptionalDouble(mapper);
+		
+		// Checks if the mapper is applied
+		Assert.assertTrue(mod2.getOptionalDouble().isPresent());
+		Assert.assertEquals(d * 2, mod2.getOptionalDouble().getAsDouble(), 0d);
+		
+		MappableOptionalDouble mod3 = MappableOptionalDouble.empty();
+		MappableOptionalDouble mod4 = mod3.flatMapAsMappableOptionalDouble(mapper);
+		
+		// Checks if the mapper is not applied
+		Assert.assertFalse(mod4.getOptionalDouble().isPresent());
+		
 	}
 }
